d838508c6a35d1c6a2b54ec530bcc274e8cfb48a
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2018-03-28 15:29:21 +0500

    Perform preliminary analysis of rolling 6-sided die 1000 times

--- pygal/die_visual.py
+++ pygal/die_visual.py
@@ -5,12 +5,24 @@ die = Die()
 
 #Make some rolls, and store results in a list
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 
+#Make a set of all unique values in the results list
 unique_values = set(results)
-print(results)
+
+#Print max, min, and all unique values to test class is working
 print("Max value in results: " + str(max(results)))
 print("Min value in results: " + str(min(results)))
 print("Unique values:\t" + str(unique_values))
+
+#Analyze the results
+frequencies = []
+for value in range(1, die.num_sides + 1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+print(frequencies)
+
+
+
