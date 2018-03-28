c8c0fa12bda1d8a8949631cbe7e1c79a6fc07f0f
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2018-03-28 16:02:49 +0500

    Create bar chart to represent results of rolling D6 1000 times

--- pygal/die_visual.py
+++ pygal/die_visual.py
@@ -1,3 +1,5 @@
+import pygal
+
 from die import Die
 
 #Create a D6 - 6 sided die
@@ -25,4 +27,13 @@ for value in range(1, die.num_sides + 1):
 print(frequencies)
 
 
+#Generating a bar graph using pygal
+hist = pygal.Bar()
+hist.title = "Results of rolling one D6 1000 times".title()
+hist.x_labels = [str(x) for x in range(1, 7)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
 
+#Adding data to bar graph and saving to file
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
